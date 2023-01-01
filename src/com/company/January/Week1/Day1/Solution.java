613f25dcc52c54fa1e0fde742e2799e2e95c4aa2
akritkbehera <84931167+akritkbehera@users.noreply.github.com>
2023-01-02 04:34:36 +0530

    First Day Done

--- src/com/company/January/Week1/Day1/Solution.java
+++ src/com/company/January/Week1/Day1/Solution.java
@@ -1,4 +1,33 @@
 package com.company.January.Week1.Day1;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static boolean wordPattern(String pattern, String s){
+        String arr[] = s.split(" ");
+        if (arr.length != pattern.length())
+            return false;
+        String a[] = new String[26];
+        for (int i = 0; i < arr.length; i++) {
+            a[pattern.charAt(i) - 'a'] = arr[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (!a[pattern.charAt(i) - 'a'].equals(arr[i]))
+                return false;
+        }
+        for (int i = 0; i < 26; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                if (a[i] != null && a[j] != null && a[i].equals(a[j]))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String pattern = "aaaa";
+        String s = "dog cat cat dog";
+        boolean bool =  wordPattern(pattern,s);
+        System.out.println(bool);
+    }
 }
